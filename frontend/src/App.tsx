67eae033e90d0b5eddf9d70cb8fc2781e69fa8c9
Joao Victor <luffy2033@outlook.com>
2022-01-12 19:53:34 -0300

    config routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,23 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from './pages/Listing';
+import Form from './pages/Form';
 import Navbar from "./assets/components/Navbar";
 
 function App() {
   return (
-    <Navbar />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":livroId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/assets/components/Navbar/index.tsx
+++ frontend/src/assets/components/Navbar/index.tsx
@@ -6,7 +6,7 @@ function Navbar(){
         <header>
     <nav className="container">
         <div className="dsmovie-nav-content">
-            <h1>DSMovie</h1>
+            <h1>JVLivros</h1>
             <a href="https://github.com/yuxis445" target="_blank" rel="noreferrer">
                 <div className="dsmovie-contact-container">
                     <GithubIcon />
