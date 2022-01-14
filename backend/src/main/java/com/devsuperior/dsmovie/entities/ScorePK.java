d1198ca1bcd9663fe8dd7c89981b390089591c8f
Joao Victor <luffy2033@outlook.com>
2022-01-13 22:09:56 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/ScorePK.java
@@ -0,0 +1,41 @@
+package com.devsuperior.dsmovie.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Embeddable
+public class ScorePK implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @ManyToOne
+    @JoinColumn(name = "livro_id")
+    private Livros livro;
+    
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public ScorePK(){}
+
+    public Livros getLivro() {
+        return livro;
+    }
+
+    public void setLivro(Livros livro) {
+        this.livro = livro;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    
+}
---
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/User.java
@@ -0,0 +1,42 @@
+package com.devsuperior.dsmovie.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+    
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+
+    public User(){}
+
+    public User(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    
+}
